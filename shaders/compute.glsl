4220a511f73977226b3d0b495e8aa610ca19c7f2
Jakub Cerveny <jakub.cerveny@gmail.com>
2018-07-11 21:38:38 +0200

    Work group size 8x8 seems to run a tiny bit smoother.

--- shaders/compute.glsl
+++ shaders/compute.glsl
@@ -1,6 +1,6 @@
 #line 2
 
-layout (local_size_x = 4, local_size_y = 4) in;
+layout (local_size_x = 8, local_size_y = 8) in;
 layout (rgba32f, binding = 0) uniform image2D img_out;
 
 uniform vec2 center;
